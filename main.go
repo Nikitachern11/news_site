2130308320e97be8a6afd56bd61af488db741b25
Nikita <nikita@example.com>
2025-03-23 22:01:39 +0300

    project renewed. first connection to mysql db done

--- main.go
+++ main.go
@@ -1,44 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
-	"net/http"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	Name                  string
-	Age                   uint16
-	Money                 int16
-	Avg_grades, Happiness float64
-	Hobbies               []string
-}
-
-func (u User) getAllinfo() string {
-	return fmt.Sprintf("Username is %s. He is %d and he has Money equal %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
-}
-
-func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 4.2, Happiness: 0.8, Hobbies: []string{"Footbal", "Dance"}}
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
-}
-
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contacts page")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts", contacts_page)
-	http.ListenAndServe(":8080", nil)
-}
-
 func main() {
+	fmt.Println("Mysql in Go")
 
-	handleRequest()
+	db, err := sql.Open("mysql", "admin:phpmyadmin@tcp(127.0.0.1:3306)/news_site")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to MySQL")
 }
